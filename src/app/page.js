237117fe23dd5521012a586f446dd887c8955343
farahbed <farah.bendriss.la@gmail.com>
2025-01-27 14:35:15 +0100

    delete file pricing.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Header from './components/home/Header';
 import Footer from './components/home/Footer';
-import SitePresentation from './components/home/SitePresentation';
-import Pricing from './components/home/Pricing';
 import UpcomingEvent from './components/home/UpcomingEvent';
 import SpeedDatingHomePage from './components/home/SpeedDatingHomePage';
 
